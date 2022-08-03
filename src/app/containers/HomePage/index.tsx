16b940940c8c9f5cc63d51a8268b42e009d080d9
Felipe Botelho <felipebotelhofelipe@hotmail.com>
2022-08-03 11:15:04 -0300

    Incluido steps e aboutUs components

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,7 +1,10 @@
 import styled from "styled-components";
 import tw from "twin.macro";
 import { BookCard } from "../../components/bookCard";
+import { Marginer } from "../../components/marginer";
 import { Navbar } from "../../components/navbar";
+import { AboutUs } from "./aboutUs";
+import { BookingSteps } from "./bookingSteps";
 import { TopSection } from "./topSection";
 
 const PageContainer = styled.div`
@@ -20,7 +23,12 @@ export default function HomePage() {
     <PageContainer>
       <Navbar />
       <TopSection />
+      <Marginer direction="vertical" margin="4em" />
       <BookCard />
+      <Marginer direction="vertical" margin="10em" />
+      <BookingSteps />
+      <Marginer direction="vertical" margin="8em" />
+      <AboutUs />
     </PageContainer>
   );
 }
